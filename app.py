070bc68220a401d09f08c9e253d4e838c960a4f2
cadabraaa <151050186+cadabraaa@users.noreply.github.com>
2024-01-22 16:29:51 +0000

    database fixed

--- app.py
+++ app.py
@@ -10,68 +10,78 @@ previous_timestamp = None
 
 scheduler = BackgroundScheduler()
 
+
 def extract_timestamp(data):
-    """
+  """
     Extract timestamp from data structure.
     """
-    timestamp = data.get('timestamp')
+  timestamp = data.get('timestamp')
 
-    if timestamp:
-        print("Extracted Timestamp:", timestamp)
-        return timestamp
+  if timestamp:
+    print("Extracted Timestamp:", timestamp)
+    return timestamp
 
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if isinstance(value, (dict, list)):
-                result = extract_timestamp(value)
-                if result:
-                    return result
-    elif isinstance(data, list):
-        for item in data:
-            result = extract_timestamp(item)
-            if result:
-                return result
+  if isinstance(data, dict):
+    for key, value in data.items():
+      if isinstance(value, (dict, list)):
+        result = extract_timestamp(value)
+        if result:
+          return result
+  elif isinstance(data, list):
+    for item in data:
+      result = extract_timestamp(item)
+      if result:
+        return result
 
-    return None
+  return None
+
 
 def job():
-    global previous_timestamp
+  global previous_timestamp
 
-    # URL of the NSE India API
-    url = 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY'
+  # URL of the NSE India API
+  url = 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY'
 
-    # Set headers to mimic a browser request
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    }
+  # Set headers to mimic a browser request
+  headers = {
+      'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+  }
 
-    # Make a GET request with headers
-    response = requests.get(url, headers=headers)
+  # Make a GET request with headers
+  response = requests.get(url, headers=headers)
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON content of the response
-        data = response.json()
+  # Check if the request was successful (status code 200)
+  if response.status_code == 200:
+    # Parse the JSON content of the response
+    data = response.json()
 
-        # Extract the timestamp using a recursive approach
-        current_timestamp = extract_timestamp(data)
+    # Extract the timestamp using a recursive approach
+    current_timestamp = extract_timestamp(data)
 
-        if current_timestamp and current_timestamp != previous_timestamp:
-            # Update the previous timestamp
-            previous_timestamp = current_timestamp
+    if current_timestamp and current_timestamp != previous_timestamp:
+      # Update the previous timestamp
+      previous_timestamp = current_timestamp
 
-            # Insert data into the database
-            insert_data_into_database(data)
+      timestamp_value = data['records']['timestamp']
+      for dict_temp in data['filtered']['data']:
+        dict_temp['PE']['timestamp'] = timestamp_value
+        dict_temp['CE']['timestamp'] = timestamp_value
+
+      # Insert data into the database
+      insert_data_into_database(data)
+      
+
 
 # Schedule the job to run every 60 seconds
-scheduler.add_job(job, 'interval', seconds=60)
+scheduler.add_job(job, 'interval', seconds=5)
 scheduler.start()
+
 
 @app.route("/", methods=['GET'])
 def index():
-    return render_template("index.html")
+  return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)
